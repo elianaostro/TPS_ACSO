38813239268989db13f9c1d1092fa67e7f33e203
Eliana Ostro <elianaostro@gmail.com>
2025-04-16 03:49:51 -0300

    ej 1 listo

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -1,15 +1,63 @@
 #include "ej1.h"
 
+// Inicializa una estructura de lista
 string_proc_list* string_proc_list_create(void){
+	string_proc_list* list = malloc(sizeof(string_proc_list));
+	if (!list) return NULL;
+	
+	list->first = NULL;
+	list->last  = NULL;
+	return list;
 }
 
+
+// Inicializa un nodo con el tipo y el hash dado.
+// El nodo tiene que apuntar al hash pasado por parámetro (no hay que copiarlo).
 string_proc_node* string_proc_node_create(uint8_t type, char* hash){
+	string_proc_node* node = malloc(sizeof(string_proc_node));
+	if (!node) return NULL;
+	
+	node->next      = NULL;
+	node->previous  = NULL;
+	node->hash      = hash;
+	node->type      = type;			
+	return node;
 }
 
+// Agrega un nodo nuevo al final de la lista con el tipo y el hash dado.
+// Recordar que no se debe copiar el hash, sino apuntar al mismo.
 void string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash){
+	string_proc_node* node = string_proc_node_create(type, hash);
+	if (!node) return;
+	
+	if (list->first == NULL) {
+		list->first = node;
+	} else {
+		list->last->next = node;
+		node->previous   = list->last;
+	}
+	list->last = node;
 }
 
+// Genera un nuevo hash concatenando el pasado por parámetro con todos los hashes
+// de los nodos de la lista cuyos tipos coinciden con el pasado por parámetro
 char* string_proc_list_concat(string_proc_list* list, uint8_t type , char* hash){
+	string_proc_node* current_node = list->first;
+	char* result = malloc(1);
+	result[0] = '\0';
+	
+	while(current_node != NULL){
+		if (current_node->type == type) {
+			char* new_result = str_concat(result, current_node->hash);
+			free(result);
+			result = new_result;
+		}
+		current_node = current_node->next;
+	}
+	
+	char* final_result = str_concat(hash, result);
+	free(result);
+	return final_result;
 }
 
 
